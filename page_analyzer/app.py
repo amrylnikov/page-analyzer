1f12b0f9a95cdfdccb148223b0e3d50d6b6ae157
amrylnikov <amrylnikov@gmail.com>
2023-07-20 18:47:50 +0300

    added one finction

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -99,12 +99,7 @@ def url_check(id):
 
             code, h1, title, description = parse(r)
             date1 = date.today()
-            cursor.execute('''INSERT INTO url_checks
-                        (url_id, status_code, h1, title, description, created_at)
-                        VALUES ('{}', '{}', '{}', '{}', '{}', '{}')
-                        ;'''.format(id, code, h1, title, description, date1))
-            cursor.execute("SELECT * FROM url_checks WHERE url_id = '{}'".format(id))
-            checks = cursor.fetchall()
+            checks = db.create_check(id, code, h1, title, description, date1)
             flash('Страница успешно проверена', 'success')
         except Exception:
             checks = []
