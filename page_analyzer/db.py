e5c936856e22a70573f9e7ca75c93478f80baf07
amrylnikov <amrylnikov@gmail.com>
2023-08-01 13:33:06 +0300

    reverted previous 2 changes

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -3,40 +3,26 @@ from datetime import date
 
 def get_url_by_id(conn, id):
     with conn.cursor() as cursor:
-        cursor.execute("""
-                    SELECT id, name, created_at
-                    FROM urls
-                    WHERE id = %s
-                    """, (id,))
+        cursor.execute("SELECT id, name, created_at FROM urls WHERE id = %s", (id,))
         return cursor.fetchone()
 
 
 def get_url_checks_by_id(conn, id):
     with conn.cursor() as cursor:
-        cursor.execute("""
-                    SELECT id, url_id, status_code,
-                       h1, title, description, created_at
-                    FROM url_checks
-                    WHERE url_id = %s
-                    """, (id,))
+        cursor.execute("SELECT id, url_id, status_code, h1, title, description, created_at FROM url_checks WHERE url_id = %s", (id,))
         return cursor.fetchall()
 
 
-def get_url_by_name(conn, name):
+def get_url_id_by_name(conn, name):
     with conn.cursor() as cursor:
-        cursor.execute("""
-                    SELECT id, name, created_at
-                    FROM urls
-                    WHERE name = %s
-                    """, (name,))
+        cursor.execute("SELECT id FROM urls WHERE name = %s", (name,))
         return cursor.fetchone()
 
 
 def get_all_url_checks(conn):
     with conn.cursor() as cursor:
         cursor.execute("""
-                    SELECT DISTINCT ON (urls.id) urls.id, urls.name,
-                       url_checks.created_at, url_checks.status_code
+                    SELECT DISTINCT ON (urls.id) urls.id, urls.name, url_checks.created_at, url_checks.status_code
                     FROM urls
                     LEFT JOIN url_checks ON url_checks.url_id = urls.id
                     ORDER BY urls.id;
@@ -47,21 +33,16 @@ def get_all_url_checks(conn):
 def create_url(conn, name):
     creation_date = date.today()
     with conn.cursor() as cursor:
-        cursor.execute("""
-                    INSERT INTO urls (name, created_at)
-                    VALUES (%s, %s)
-                    RETURNING id;
-                    """, (name, creation_date))
+        cursor.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;", (name, creation_date))
         return cursor.fetchone()[0]
 
 
 def create_check(conn, id, code, h1, title, description):
     with conn.cursor() as cursor:
         creation_date = date.today()
-        cursor.execute("""
-                    INSERT INTO url_checks
+        cursor.execute('''INSERT INTO url_checks
                     (url_id, status_code, h1, title, description, created_at)
                     VALUES (%s, %s, %s, %s, %s, %s)
-                    ;""", (id, code, h1, title, description, creation_date))
+                    ;''', (id, code, h1, title, description, creation_date))
         cursor.execute("SELECT * FROM url_checks WHERE url_id = %s", (id,))
         return cursor.fetchall()
