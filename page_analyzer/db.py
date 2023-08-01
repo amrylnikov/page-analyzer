7861b0123bd176ad769d29d237c286a2b1ffb122
amrylnikov <amrylnikov@gmail.com>
2023-08-01 13:51:08 +0300

    split requests again

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -3,30 +3,40 @@ from datetime import date
 
 def get_url_by_id(conn, id):
     with conn.cursor() as cursor:
-        cursor.execute("SELECT id, name, created_at FROM urls WHERE id = %s", (id,))
+        cursor.execute("""
+                    SELECT id, name, created_at
+                    FROM urls
+                    WHERE id = %s;
+                    """, (id,))
         return cursor.fetchone()
 
 
 def get_url_checks_by_id(conn, id):
     with conn.cursor() as cursor:
-        cursor.execute("SELECT id, url_id, status_code, h1, title, description, created_at FROM url_checks WHERE url_id = %s", (id,))
+        cursor.execute("""
+                    SELECT id, url_id, status_code, h1, title, description,
+                       created_at
+                    FROM url_checks
+                    WHERE url_id = %s;
+                    """, (id,))
         return cursor.fetchall()
 
 
 def get_url_by_name(conn, name):
     with conn.cursor() as cursor:
         cursor.execute("""
-                       SELECT id, name, created_at
-                       FROM urls
-                       WHERE name = %s
-                       """, (name,))
+                    SELECT id, name, created_at
+                    FROM urls
+                    WHERE name = %s;
+                    """, (name,))
         return cursor.fetchall()
 
 
 def get_all_url_checks(conn):
     with conn.cursor() as cursor:
         cursor.execute("""
-                    SELECT DISTINCT ON (urls.id) urls.id, urls.name, url_checks.created_at, url_checks.status_code
+                    SELECT DISTINCT ON (urls.id) urls.id, urls.name,
+                       url_checks.created_at, url_checks.status_code
                     FROM urls
                     LEFT JOIN url_checks ON url_checks.url_id = urls.id
                     ORDER BY urls.id;
@@ -37,7 +47,11 @@ def get_all_url_checks(conn):
 def create_url(conn, name):
     creation_date = date.today()
     with conn.cursor() as cursor:
-        cursor.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s) RETURNING id;", (name, creation_date))
+        cursor.execute("""
+                    INSERT INTO urls (name, created_at) 
+                    VALUES (%s, %s) 
+                    RETURNING id;
+                    """, (name, creation_date))
         return cursor.fetchone()[0]
 
 
