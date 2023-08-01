dc05e81d2e007299a3b34146040c535dcec393ab
amrylnikov <amrylnikov@gmail.com>
2023-08-01 12:53:04 +0300

    changed id in SQL to SERIAL

--- database.sql
+++ database.sql
@@ -1,11 +1,11 @@
 CREATE TABLE urls (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     name varchar(255),
     created_at date
 );
 
 CREATE TABLE url_checks (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     url_id bigint REFERENCES urls (id),
     status_code integer,
     h1 varchar(255),
